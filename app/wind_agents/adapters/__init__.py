9bd1a2308ec0fcdd25ca1c43e7e4db55d91aa5a3
EmAnzi3 <emanueleanzilotti@hotmail.com>
2026-09-05 21:22:02 +0200

    Wind v0.6: register Sardegna wind agent

--- app/wind_agents/adapters/__init__.py
+++ app/wind_agents/adapters/__init__.py
@@ -1,11 +1,13 @@
 from .lazio import LazioWindAgent
 from .mase import MaseWindAgent
+from .sardegna import SardegnaWindAgent
 from .toscana import ToscanaWindAgent
 from .toscana_atos import ToscanaAtosWindAgent
 
 __all__ = [
     "LazioWindAgent",
     "MaseWindAgent",
+    "SardegnaWindAgent",
     "ToscanaWindAgent",
     "ToscanaAtosWindAgent",
 ]
