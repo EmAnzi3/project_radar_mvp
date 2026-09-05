bec5bf92257e12e7c199bea3df00b534094b3710
EmAnzi3 <emanueleanzilotti@hotmail.com>
2026-09-05 22:02:08 +0200

    Register five additional regional wind adapters

--- app/wind_agents/adapters/__init__.py
+++ app/wind_agents/adapters/__init__.py
@@ -1,27 +1,37 @@
 from .basilicata import BasilicataWindAgent
 from .calabria import CalabriaWindAgent
 from .campania import CampaniaWindAgent
+from .emilia_romagna import EmiliaRomagnaWindAgent
 from .lazio import LazioWindAgent
+from .lombardia import LombardiaWindAgent
 from .mase import MaseWindAgent
 from .mase_provvedimenti import MaseProvvedimentiWindAgent
+from .piemonte import PiemonteWindAgent
 from .sardegna import SardegnaWindAgent
 from .sicilia import SiciliaWindAgent
 from .sistema_puglia import SistemaPugliaWindAgent
 from .terna_econnextion import TernaEconnextionWindAgent
 from .toscana import ToscanaWindAgent
 from .toscana_atos import ToscanaAtosWindAgent
+from .umbria import UmbriaWindAgent
+from .veneto import VenetoWindAgent
 
 __all__ = [
     "BasilicataWindAgent",
     "CalabriaWindAgent",
     "CampaniaWindAgent",
+    "EmiliaRomagnaWindAgent",
     "LazioWindAgent",
+    "LombardiaWindAgent",
     "MaseWindAgent",
     "MaseProvvedimentiWindAgent",
+    "PiemonteWindAgent",
     "SardegnaWindAgent",
     "SiciliaWindAgent",
     "SistemaPugliaWindAgent",
     "TernaEconnextionWindAgent",
     "ToscanaWindAgent",
     "ToscanaAtosWindAgent",
+    "UmbriaWindAgent",
+    "VenetoWindAgent",
 ]
