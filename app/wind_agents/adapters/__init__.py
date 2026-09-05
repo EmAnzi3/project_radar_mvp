b5cb708ffb02da8ce4028babfc780b54ee4c0e78
EmAnzi3 <emanueleanzilotti@hotmail.com>
2026-09-05 21:23:35 +0200

    Wind v0.6: register Sicilia wind agent

--- app/wind_agents/adapters/__init__.py
+++ app/wind_agents/adapters/__init__.py
@@ -1,6 +1,7 @@
 from .lazio import LazioWindAgent
 from .mase import MaseWindAgent
 from .sardegna import SardegnaWindAgent
+from .sicilia import SiciliaWindAgent
 from .toscana import ToscanaWindAgent
 from .toscana_atos import ToscanaAtosWindAgent
 
@@ -8,6 +9,7 @@ __all__ = [
     "LazioWindAgent",
     "MaseWindAgent",
     "SardegnaWindAgent",
+    "SiciliaWindAgent",
     "ToscanaWindAgent",
     "ToscanaAtosWindAgent",
 ]
