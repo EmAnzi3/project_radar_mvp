a42c3354f3db53e2ead3aee27ffe048a9c61a9eb
EmAnzi3 <emanueleanzilotti@hotmail.com>
2026-09-05 22:21:27 +0200

    wind v0.6: register regional gap adapters

--- app/wind_agents/adapters/__init__.py
+++ app/wind_agents/adapters/__init__.py
@@ -1,11 +1,15 @@
+from .abruzzo import AbruzzoWindAgent
 from .basilicata import BasilicataWindAgent
 from .calabria import CalabriaWindAgent
 from .campania import CampaniaWindAgent
 from .emilia_romagna import EmiliaRomagnaWindAgent
 from .lazio import LazioWindAgent
+from .liguria import LiguriaWindAgent
 from .lombardia import LombardiaWindAgent
+from .marche import MarcheWindAgent
 from .mase import MaseWindAgent
 from .mase_provvedimenti import MaseProvvedimentiWindAgent
+from .molise import MoliseWindAgent
 from .piemonte import PiemonteWindAgent
 from .sardegna import SardegnaWindAgent
 from .sicilia import SiciliaWindAgent
@@ -17,14 +21,18 @@ from .umbria import UmbriaWindAgent
 from .veneto import VenetoWindAgent
 
 __all__ = [
+    "AbruzzoWindAgent",
     "BasilicataWindAgent",
     "CalabriaWindAgent",
     "CampaniaWindAgent",
     "EmiliaRomagnaWindAgent",
     "LazioWindAgent",
+    "LiguriaWindAgent",
     "LombardiaWindAgent",
+    "MarcheWindAgent",
     "MaseWindAgent",
     "MaseProvvedimentiWindAgent",
+    "MoliseWindAgent",
     "PiemonteWindAgent",
     "SardegnaWindAgent",
     "SiciliaWindAgent",
